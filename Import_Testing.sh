392445c313da2a0c6e3fe56f8632a6bc49f7aede
mkmsit005 <52464577+mkmsit005@users.noreply.github.com>
2019-09-19 14:39:02 +0530

    Update Import_Testing.sh

--- Import_Testing.sh
+++ Import_Testing.sh
@@ -1,18 +1,7 @@
-#!/bin/bash
-
-BINPATH=$1
-WORKFLOWNAME=$2
-CNTRLFILE=$3
-REPOSITORY=$4 
-INFA_DOMAIN=$5 
-INFA_USERNAME=$6
-INFA_PWD=$7
-
-
-cd $BINPATH
-pmrep connect -r $REPOSITORY -d $INFA_DOMAIN -n $INFA_USERNAME -x $INFA_PWD
-echo $WORKFLOWNAME
-pmrep objectimport -i $BINPATH/$WORKFLOWNAME -c $BINPATH/$CNTRLFILE -l $BINPATH/import.log
+cd /local1/infapc/10.1.1/server/bin
+pmrep connect -r PCREP_US_DEV -d INFA_DOM_DEV -n infa_admin -x admin
+echo 'wf_m_testing.XML'
+pmrep objectimport -i /local1/infapc/10.1.1/server/bin/wf_m_testing.XML -c /local1/infapc/10.1.1/server/bin/TEST.txt -l /local1/infapc/10.1.1/server/bin/import.log
 echo "Job Import Successful"
 
 
