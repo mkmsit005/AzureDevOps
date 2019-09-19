3b9fa9cf21601f20536e06660064a37e81548df1
mkmsit005 <52464577+mkmsit005@users.noreply.github.com>
2019-09-19 19:23:56 +0530

    Update Import_Testing.sh

--- Import_Testing.sh
+++ Import_Testing.sh
@@ -1,5 +1,5 @@
 echo 'wf_m_testing.XML'
-cd '/local1/infapc/10.1.1/server/bin'
+cd /local1/infapc/10.1.1/server/bin
 pwd
 pmrep connect -r PCREP_US_DEV -d INFA_DOM_DEV -n infa_admin -x admin
 pmrep objectimport -i /local1/infapc/10.1.1/server/bin/wf_m_testing.XML -c /local1/infapc/10.1.1/server/bin/TEST.txt -l /local1/infapc/10.1.1/server/bin/import.log
