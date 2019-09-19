28f67de239f56c1fe1ee81b2a8fba8a443d817f8
mkmsit005 <52464577+mkmsit005@users.noreply.github.com>
2019-09-19 15:00:20 +0530

    Update Import_Testing.sh

--- Import_Testing.sh
+++ Import_Testing.sh
@@ -4,10 +4,3 @@ echo 'wf_m_testing.XML'
 echo 'wf_m_testing.XML'
 #pmrep objectimport -i /local1/infapc/10.1.1/server/bin/wf_m_testing.XML -c /local1/infapc/10.1.1/server/bin/TEST.txt -l /local1/infapc/10.1.1/server/bin/import.log
 echo 'Job Import Successful'
-
-
-
-
-
-
-
